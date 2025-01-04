fe10cab790c50cf1a7b60d41a46f30afe69026b8
chetanbawane26 <bawanechetan123@gmail.com>
2025-01-05 01:12:34 +0530

    second commit

---
+++ DoctorPaitent/src/main/java/com/example/demo/entity/Doctor.java
@@ -0,0 +1,82 @@
+package com.example.demo.entity;
+
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Table
+@Entity(name = "doctor")
+public class Doctor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int doctor_id;
+	@Column
+	private int experience;
+	@Column
+	private String name ,degree,specialization;
+	
+	@OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL)
+	private List<Patient> paitents;
+
+	public int getDoctor_id() {
+		return doctor_id;
+	}
+
+	public void setDoctor_id(int doctor_id) {
+		this.doctor_id = doctor_id;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public List<Patient> getPaitents() {
+		return paitents;
+	}
+
+	public void setPaitents(List<Patient> paitents) {
+		this.paitents = paitents;
+	}
+	
+	
+	
+}
---
+++ DoctorPaitent/src/main/java/com/example/demo/entity/Patient.java
@@ -0,0 +1,92 @@
+package com.example.demo.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Table
+@Entity(name = "patient")
+public class Patient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	
+	@Column
+	String fname,lname,address,contact; 
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date birthdate;
+	
+	@ManyToOne
+	@JoinColumn(name = "doctor_id")
+	private Doctor doctor;
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Doctor getDoctor_id() {
+		return doctor;
+	}
+
+	public void setDoctor_id(Doctor doctor_id) {
+		this.doctor = doctor_id;
+	}
+	
+	
+}
